509fc6f36e61c8223d052fba00be774e6622ea33
canela7 <canelabrian123@gmail.com>
2018-06-01 16:36:25 -0700

    login set up with constraints

--- Haircision/Controller/HaircisionIntroViewController.swift
+++ Haircision/Controller/HaircisionIntroViewController.swift
@@ -29,15 +29,6 @@ class HaircisionIntroViewController: UIViewController {
         // Show the navigation bar on other view controllers
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
